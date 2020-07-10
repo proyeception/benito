c0c1ba3cd8d6fcf90cc23da6aa94d81c85c6dff6
Alejandro <aleperaltabazas@hotmail.com>
2020-07-10 13:18:40 -0300

    Searchbox bien rústico

--- benito-frontend/src/components/Search/index.tsx
+++ benito-frontend/src/components/Search/index.tsx
@@ -1,16 +1,22 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 import ProjectSummary from "./ProjectSummary";
+import SearchBox from "./SearchBox";
 
 const Search = (_: any) => (
-  <div>
-    <div className="qui-search-header p-2 pl-4 qui-font-title">
-      <div>Proyectos</div>
+  <div className="row">
+    <div className="col-md-2 qui-searchbox-md">
+      <SearchBox />
     </div>
-    <div className=""></div>
-    <ProjectSummary />
-    <hr />
-    <ProjectSummary />
+    <div className="col-md-10">
+      <div className="qui-search-header p-2 pl-4 qui-font-title">
+        <div>Proyectos</div>
+      </div>
+      <div className=""></div>
+      <ProjectSummary />
+      <hr />
+      <ProjectSummary />
+    </div>
   </div>
 );
 
