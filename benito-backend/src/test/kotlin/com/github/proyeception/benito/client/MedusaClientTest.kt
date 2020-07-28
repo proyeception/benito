d93b999c7751881d4ea9dea1174e3d82e90fcbd5
matigiorda <matiasgiorda27@gmail.com>
2020-07-28 17:59:18 -0300

    Agrego test de ordenamiento en la interacción con Medusa

--- benito-backend/src/test/kotlin/com/github/proyeception/benito/client/MedusaClientTest.kt
+++ benito-backend/src/test/kotlin/com/github/proyeception/benito/client/MedusaClientTest.kt
@@ -3,6 +3,7 @@ package com.github.proyeception.benito.client
 import com.fasterxml.jackson.core.type.TypeReference
 import com.github.proyeception.benito.connector.Connector
 import com.github.proyeception.benito.connector.Response
+import com.github.proyeception.benito.dto.OrderDTO
 import com.github.proyeception.benito.dto.PersonDTO
 import com.github.proyeception.benito.dto.ProjectDTO
 import com.github.proyeception.benito.exception.FailedDependencyException
@@ -48,7 +49,7 @@ class MedusaClientTest : WordSpec() {
             "get to /projects returns all projects" {
                 val projectsResponse = listOf<ProjectDTO>(project)
 
-                on(medusaConnector.get(eq("/projects?"))).thenReturn(responseMock)
+                on(medusaConnector.get(eq(STRAPI_PROJECTS_PATH))).thenReturn(responseMock)
                 on(responseMock.isError()).thenReturn(false)
                 on(responseMock.deserializeAs(ArgumentMatchers.any(TypeReference::class.java))).thenReturn(projectsResponse)
 
@@ -59,13 +60,84 @@ class MedusaClientTest : WordSpec() {
             }
 
             "throw if medusa returns error" {
-                on(medusaConnector.get(eq("/projects?"))).thenReturn(responseMock)
+                on(medusaConnector.get(eq(STRAPI_PROJECTS_PATH))).thenReturn(responseMock)
                 on(responseMock.isError()).thenReturn(true)
 
                 shouldThrow<FailedDependencyException> {
                     medusaClient.projects()
                 }
             }
+
+            "should get to strapi's ascending date sort when using DATE_ASC" {
+                val projectsResponse = listOf<ProjectDTO>(project)
+
+                val expectedEndpoint =
+                        STRAPI_PROJECTS_PATH + STRAPI_SORT_QUERY_PARAM + STRAPI_DATE_FIELD + STRAPI_ASC_IDENTIFIER
+                on(medusaConnector.get(eq(expectedEndpoint))).thenReturn(responseMock)
+                on(responseMock.isError()).thenReturn(false)
+                on(responseMock.deserializeAs(ArgumentMatchers.any(TypeReference::class.java))).thenReturn(projectsResponse)
+
+                val expected = projectsResponse
+                val actual = medusaClient.projects(OrderDTO.DATE_ASC)
+
+                expected shouldBe actual
+            }
+
+            "should get to strapi's descending date sort when using DATE_DESC" {
+                val projectsResponse = listOf<ProjectDTO>(project)
+
+                val expectedEndpoint =
+                        STRAPI_PROJECTS_PATH + STRAPI_SORT_QUERY_PARAM + STRAPI_DATE_FIELD + STRAPI_DESC_IDENTIFIER
+                on(medusaConnector.get(eq(expectedEndpoint))).thenReturn(responseMock)
+                on(responseMock.isError()).thenReturn(false)
+                on(responseMock.deserializeAs(ArgumentMatchers.any(TypeReference::class.java))).thenReturn(projectsResponse)
+
+                val expected = projectsResponse
+                val actual = medusaClient.projects(OrderDTO.DATE_DESC)
+
+                expected shouldBe actual
+            }
+
+            "should get to strapi's ascending alphabetic sort when using ALPHA_ASC" {
+                val projectsResponse = listOf<ProjectDTO>(project)
+
+                val expectedEndpoint =
+                        STRAPI_PROJECTS_PATH + STRAPI_SORT_QUERY_PARAM + STRAPI_ALPHABETIC_FIELD + STRAPI_ASC_IDENTIFIER
+                on(medusaConnector.get(eq(expectedEndpoint))).thenReturn(responseMock)
+                on(responseMock.isError()).thenReturn(false)
+                on(responseMock.deserializeAs(ArgumentMatchers.any(TypeReference::class.java))).thenReturn(projectsResponse)
+
+                val expected = projectsResponse
+                val actual = medusaClient.projects(OrderDTO.ALPHA_ASC)
+
+                expected shouldBe actual
+            }
+
+            "should get to strapi's descending alphabetic sort when using ALPHA_DESC" {
+                val projectsResponse = listOf<ProjectDTO>(project)
+
+                val expectedEndpoint =
+                        STRAPI_PROJECTS_PATH + STRAPI_SORT_QUERY_PARAM + STRAPI_ALPHABETIC_FIELD + STRAPI_DESC_IDENTIFIER
+                on(medusaConnector.get(eq(expectedEndpoint))).thenReturn(responseMock)
+                on(responseMock.isError()).thenReturn(false)
+                on(responseMock.deserializeAs(ArgumentMatchers.any(TypeReference::class.java))).thenReturn(projectsResponse)
+
+                val expected = projectsResponse
+                val actual = medusaClient.projects(OrderDTO.ALPHA_DESC)
+
+                expected shouldBe actual
+            }
+
         }
+    }
+
+    companion object {
+        const val STRAPI_PROJECTS_PATH = "/projects?"
+        const val STRAPI_SORT_QUERY_PARAM = "_sort="
+        const val STRAPI_ASC_IDENTIFIER = ":ASC"
+        const val STRAPI_DESC_IDENTIFIER = ":DESC"
+
+        const val STRAPI_DATE_FIELD = "creationDate"
+        const val STRAPI_ALPHABETIC_FIELD = "title"
     }
 }
