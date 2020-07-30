d0129f2e81697c742109f46ecaa0f46aeb5d4ad9
Ro <rociochipian@gmail.com>
2020-07-30 20:14:57 -0300

    ver un proyecto especifico start

---
+++ benito-frontend/src/components/Project/ProjectInfo.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { hot } from "react-hot-loader";
+import "./styles.scss";
+
+export type Project = {
+    id: String;
+    title: String;
+    description: String;
+    posterUrl: string;
+    authors: Array<Person>;
+    creationDate: Date;
+    tags: Array<String>
+  };
+  
+  type Person = {
+    username: String;
+    profileUrl: String;
+  };
+  
+  type Props = {
+    project: Project;
+  };
+
+const ProjectInfo = (props: Props) => (
+    <div className="row container-fluid mt-3 ml-0">
+      <div className="col-sm-12 col-md-10">
+        <div className="qui-summary-title qui-font-title">
+          {props.project.title}
+        </div>
+        <div className="d-sm-block d-md-none">
+          <img className="qui-summary-image-sm" src={props.project.posterUrl} />
+        </div>
+        <div className="qui-summary qui-font-text mt-3">
+          {props.project.description}
+        </div>
+        <div className="qui-authors">
+          {props.project.authors.map((a) => a.username).join(", ")}
+        </div>
+      </div>
+      <div className="col-md-2 d-none d-lg-block">
+        <img className="qui-summary-image-md" src={props.project.posterUrl} />
+      </div>
+    </div>
+  );
+
+  export default hot(module)(ProjectInfo);
--- benito-frontend/src/components/Project/index.tsx
+++ benito-frontend/src/components/Project/index.tsx
@@ -2,17 +2,20 @@ import React, { Component } from "react";
 import { hot } from "react-hot-loader";
 import axios from "axios";
 import { benitoHost } from "../../config";
-import ProjectSummary, { Project } from "../Search/ProjectSummary";
+import { Project } from "./ProjectInfo";
+import Header from "../Header/index"
+import "./styles.scss";
 
-let proy : Project = {id: "2", title:"un proyecto", description:"la descripcion del proyecto", posterUrl:"www.url.com", authors:[]}
+//let proy : Project = {id: "2", title:"un proyecto", description:"la descripcion del proyecto", posterUrl:"www.url.com", authors:[]}
 
-class ViewProject extends Component<{}, { project: Project } > {
+class ViewProject extends Component<{}, { projectId: String, project: Project } > {
 
     constructor(props: {}, ctx: any) {
       super(props, ctx);
-      this.state = { project: {} as Project };
+      this.state = { projectId: "5f1a0a7e3552040017fd532d", project: {id:"", title:"", description:"",
+        posterUrl:"", authors:[], creationDate:null, tags:[] } };
     }
-  
+
     componentDidMount() {
       axios.get(`${benitoHost}/benito/projects/${this.state.project.id}`).then((res) => {
         const project = res.data;
@@ -22,7 +25,20 @@ class ViewProject extends Component<{}, { project: Project } > {
   
     render() {
       return (
-        <div className="container-fluid sm-login-header"></div>
+        <div>
+          <Header/>
+          <div className="container-fluid">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="qui-proyect-title">
+                  {this.state.project.title}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       )
     }
 }
+
+export default hot(module)(ViewProject);
