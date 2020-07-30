c115baa25c4d58c18020b8b5980309fe8375793e
matigiorda <matiasgiorda27@gmail.com>
2020-07-30 00:10:54 -0300

    Agrego búsqueda que filtra resultados en base a lo cargado en la Searchbox por nombre, tags y fechas (este último básico)

--- benito-frontend/src/components/Search/SearchBox.tsx
+++ benito-frontend/src/components/Search/SearchBox.tsx
@@ -1,30 +1,40 @@
 import React from "react";
 import { hot } from "react-hot-loader";
 
-const SearchBox = (_: any) => (
+type Props = {
+  searchCallback(): void
+  nameCallback(name: string): void
+  categoryCallback(category: string): void
+  fromDateCallback(fromDate: string): void
+  toDateCallback(toDate: string): void
+  keywordCallback(keyword: string): void
+  documentationCallback(documentation: string): void
+}
+
+const SearchBox = (props: Props) => (
   <div className='qui-search-box'>
     <div className='qui-search-filter'>
       <div className='qui-font-text'>Nombre</div>
-      <input className="qui-search-input"></input>
+      <input className="qui-search-input" onChange={(e) => props.nameCallback(e.target.value)}></input>
     </div>
     <div className='qui-search-filter'>
       <div className='qui-font-text'>Categoria</div>
-      <input className="qui-search-input"></input>
+      <input className="qui-search-input" onChange={(e) => props.categoryCallback(e.target.value)}></input>
     </div>
     <div className='qui-search-filter'>
       <div className='qui-font-text'>Fechas</div>
-        <input className="qui-search-input-date"></input>
-        <input className="qui-search-input-date"></input>
+      <input className="qui-search-input-date" onChange={(e) => props.fromDateCallback(e.target.value)}></input>
+      <input className="qui-search-input-date" onChange={(e) => props.toDateCallback(e.target.value)}></input>
     </div>
     <div className='qui-search-filter'>
       <div className='qui-font-text'>Palabra clave</div>
-      <input className="qui-search-input"></input>
+      <input className="qui-search-input" onChange={(e) => props.keywordCallback(e.target.value)}></input>
     </div>
     <div className='qui-search-filter'>
       <div className='qui-font-text'>Documentación</div>
-      <input className="qui-search-input"></input>
+      <input className="qui-search-input" onChange={(e) => props.documentationCallback(e.target.value)}></input>
     </div>
-    <button className='btn-primary qui-search-btn'>Buscar</button>
+    <button className='btn-primary qui-search-btn' onClick={() => props.searchCallback()}>Buscar</button>
   </div>
 );
 
