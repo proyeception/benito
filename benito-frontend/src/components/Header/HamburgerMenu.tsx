20edd002b6e1b15e208174a495c06c75aac53ff9
Alejandro <aleperaltabazas@hotmail.com>
2020-08-08 23:57:41 -0300

    approach super naive a busqueda

--- benito-frontend/src/components/Header/HamburgerMenu.tsx
+++ benito-frontend/src/components/Header/HamburgerMenu.tsx
@@ -7,6 +7,9 @@ import { RootState } from "../../reducers";
 import { connect } from "react-redux";
 import store from "../../store";
 import { toggleHamburgerButton } from "../../actions/common";
+import { updateProjects } from "../../actions/search";
+import { Redirect } from "react-router-dom";
+import { fetchProjects } from "../../functions/search";
 
 const searchIconUrl =
   "https://cdn2.iconfinder.com/data/icons/font-awesome/1792/search-512.png";
@@ -15,33 +18,73 @@ type Props = {
   isOpen: Boolean;
 };
 
-const HamburgerMenu = (props: Props) => (
-  <div className="d-block d-md-none">
-    <Menu
-      width={296}
-      customBurgerIcon={
-        <img src={searchIconUrl} className="qui-search-icon invert-colors" />
-      }
-      isOpen={props.isOpen.valueOf()}
-      onStateChange={(it) => store.dispatch(toggleHamburgerButton(it.isOpen))}
-    >
-      <div className="container-fluid qui-mobile-search-container">
-        <div className="row no-gutters">
-          <div className="col-11" style={{ marginLeft: "-15px" }}>
-            <input
-              type="text"
-              className="form-control qui-mobile-search"
-              placeholder="Buscar"
+type State = {
+  doRedirect: Boolean;
+};
+
+class HamburgerMenu extends React.Component<Props, State> {
+  constructor(props: Props, ctx: any) {
+    super(props, ctx);
+    this.state = {
+      doRedirect: false,
+    };
+  }
+  render() {
+    if (this.state.doRedirect) {
+      this.setState({ doRedirect: false });
+      return <Redirect to="/search" />;
+    }
+
+    return (
+      <div className="d-block d-md-none">
+        <Menu
+          width={296}
+          customBurgerIcon={
+            <img
+              src={searchIconUrl}
+              className="qui-search-icon invert-colors"
             />
+          }
+          isOpen={this.props.isOpen.valueOf()}
+          onStateChange={(it) =>
+            store.dispatch(toggleHamburgerButton(it.isOpen))
+          }
+        >
+          <div className="container-fluid qui-mobile-search-container">
+            <div className="row no-gutters">
+              <div className="col-11" style={{ marginLeft: "-15px" }}>
+                <input
+                  type="text"
+                  className="form-control qui-mobile-search"
+                  placeholder="Buscar"
+                />
+              </div>
+              <div
+                className="col-1 center qui-search-icon-button"
+                style={{ marginRight: "-15px" }}
+              >
+                <img
+                  src={searchIconUrl}
+                  className="qui-search-icon-button"
+                  onClick={() => {
+                    fetchProjects()
+                      .then((res) => res.data)
+                      .then((projects) =>
+                        store.dispatch(updateProjects(projects))
+                      )
+                      .then(() => store.dispatch(toggleHamburgerButton(false)))
+                      .then(() => this.setState({ doRedirect: true }))
+                      .catch(console.error);
+                  }}
+                ></img>
+              </div>
+            </div>
           </div>
-          <div className="col-1 center" style={{ marginRight: "-15px" }}>
-            <img src={searchIconUrl} className="qui-search-icon-button" />
-          </div>
-        </div>
+        </Menu>
       </div>
-    </Menu>
-  </div>
-);
+    );
+  }
+}
 
 const mapStateToProps = (rootState: RootState) => {
   return {
