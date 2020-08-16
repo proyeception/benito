c6307d2a15971d5fde2945afc9d49ff2c914973f
Alejandro <aleperaltabazas@hotmail.com>
2020-08-16 13:58:58 -0300

    diferentes tamaños para mobile y desktop

--- benito-frontend/src/components/Project/ProjectInfo.tsx
+++ benito-frontend/src/components/Project/ProjectInfo.tsx
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactMarkdown from "react-markdown";
 import { hot } from "react-hot-loader";
 import "./styles.scss";
 import moment from "moment";
 import { Project } from "../Search/ProjectSummary";
+import ProjectTitle from "./ProjectTitle";
 
 type Props = {
   project: Project;
@@ -13,39 +14,26 @@ function Image(props: any) {
   return <img {...props} style={{ maxWidth: "100%" }} />;
 }
 
-const MAX_POSTER_HEIGHT = 150;
-const MIN_POSTER_HEIGHT = 60;
+const MAX_DESKTOP_HEIGHT = 300;
+const MIN_DESKTOP_HEIGHT = 100;
+const MAX_MOBILE_HEIGHT = 150;
+const MIN_MOBILE_HEIGHT = 60;
 
 const ProjectInfo = (props: Props) => {
-  const [height, setHeight] = useState(MAX_POSTER_HEIGHT);
-  const handleScroll = () => {
-    let scrollTop = window.scrollY;
-    setHeight(Math.max(MIN_POSTER_HEIGHT, MAX_POSTER_HEIGHT - scrollTop));
-  };
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
-
   return (
     <div className="ml-md-5 mr-md-5 mt-md-5">
-      <div style={{ zIndex: 999 }} className="qui-sticky-title">
-        <div
-          className="qui-blurred-image"
-          style={{
-            backgroundImage: `url(${props.project.posterUrl})`,
-            height: height,
-          }}
-        >
-          <div
-            className="qui-backdrop qui-project-title"
-            style={{ height: height }}
-          >
-            {props.project.title}
-          </div>
-        </div>
-      </div>
+      <ProjectTitle
+        project={props.project}
+        maxHeight={MAX_DESKTOP_HEIGHT}
+        minHeight={MIN_DESKTOP_HEIGHT}
+        display="d-none d-md-block"
+      />
+      <ProjectTitle
+        project={props.project}
+        maxHeight={MAX_MOBILE_HEIGHT}
+        minHeight={MIN_MOBILE_HEIGHT}
+        display="d-block d-md-none"
+      />
 
       <div className="container-fluid qui-box">
         <div className="row">
---
+++ benito-frontend/src/components/Project/ProjectTitle.tsx
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { hot } from "react-hot-loader";
+import "./styles.scss";
+import { Project } from "../Search/ProjectSummary";
+
+type Props = {
+  project: Project;
+  maxHeight: Number;
+  minHeight: Number;
+  display: String;
+};
+
+const ProjectTitle = (props: Props) => {
+  const [height, setHeight] = useState(props.maxHeight);
+  const handleScroll = () => {
+    let scrollTop = window.scrollY;
+    setHeight(
+      Math.max(props.minHeight.valueOf(), props.maxHeight.valueOf() - scrollTop)
+    );
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div
+      style={{ zIndex: 999 }}
+      className={`qui-sticky-title ${props.display}`}
+    >
+      <div
+        className="qui-blurred-image"
+        style={{
+          backgroundImage: `url(${props.project.posterUrl})`,
+          height: height.valueOf(),
+        }}
+      >
+        <div
+          className="qui-backdrop qui-project-title"
+          style={{ height: height.valueOf() }}
+        >
+          {props.project.title}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default hot(module)(ProjectTitle);
