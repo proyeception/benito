0f3229b682a154a547f338b1ffcfaea66326117e
Alejandro <aleperaltabazas@hotmail.com>
2020-09-01 17:06:58 -0300

    tooltip menu de user

--- benito-frontend/src/components/Header/User.tsx
+++ benito-frontend/src/components/Header/User.tsx
@@ -4,7 +4,8 @@ import "./styles.scss";
 import { RootState } from "../../reducers";
 import { connect } from "react-redux";
 import { clearSession, startLogin } from "../../functions/session";
-import { RouteComponentProps, withRouter } from "react-router-dom";
+import { RouteComponentProps, withRouter, Link } from "react-router-dom";
+import ReactTooltip from "react-tooltip";
 
 interface Props extends RouteComponentProps {
   token?: String;
@@ -16,7 +17,6 @@ interface Props extends RouteComponentProps {
 
 const googleIcon =
   "https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_1/v1533665793/ogdensdorg/p6ckkwxkxi8zd03ja8h0/GoogleButton.png";
-googleIcon;
 const noProfilePicture = "https://simpleicon.com/wp-content/uploads/user1.png";
 
 const User = (props: Props) => {
@@ -26,19 +26,42 @@ const User = (props: Props) => {
 
   if (props.isLoggedIn) {
     return (
-      <div
-        onClick={() => {
-          clearSession(props.token);
-          props.history.go(0);
-        }}
-        className="qui-header-user center-vertically justify-content-end pr-md-5"
-      >
-        <div className="qui-user-icon-container">
+      <div className="qui-header-user center-vertically justify-content-end pr-md-5">
+        <div
+          data-tip
+          data-for="header-user-menu"
+          data-event="click"
+          className="qui-user-icon-container"
+        >
           <img
             className="img-circle"
             src={props.profilePicture?.valueOf() || noProfilePicture}
           />
         </div>
+        <ReactTooltip
+          id="header-user-menu"
+          clickable={true}
+          place="bottom"
+          effect="solid"
+          type="light"
+          globalEventOff="click"
+        >
+          <Link
+            to={`/students/${props.userId}`}
+            style={{ textDecoration: "none" }}
+          >
+            <div className="qui-text font-size-18-md">Mi perfil</div>
+          </Link>
+          <div
+            className="qui-text font-size-18-md cursor-pointer"
+            onClick={() => {
+              clearSession(props.token);
+              props.history.go(0);
+            }}
+          >
+            Cerrar sesión
+          </div>
+        </ReactTooltip>
       </div>
     );
   }
