fa747fa84ac4c7f607b1b2460b0f849429dfb926
Alejandro <aleperaltabazas@hotmail.com>
2020-09-03 12:10:20 -0300

    quien te conoce react tooltip

--- benito-frontend/src/components/Header/User.tsx
+++ benito-frontend/src/components/Header/User.tsx
@@ -5,9 +5,11 @@ import { RootState } from "../../reducers";
 import { connect } from "react-redux";
 import { clearSession } from "../../functions/session";
 import { RouteComponentProps, withRouter, Link } from "react-router-dom";
-import ReactTooltip from "react-tooltip";
 import Login from "../Login";
 import { Role } from "../../types";
+import { Dropdown } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUser, faSignOutAlt } from "@fortawesome/free-solid-svg-icons";
 
 interface Props extends RouteComponentProps {
   token?: String;
@@ -18,8 +20,26 @@ interface Props extends RouteComponentProps {
   role: Role;
 }
 
+const CustomToggle = (props: Props) =>
+  React.forwardRef(({ onClick }: any, ref: any) => (
+    <div
+      ref={ref}
+      onClick={(e) => {
+        e.preventDefault();
+        onClick(e);
+      }}
+      className="qui-user-icon-container"
+    >
+      <img
+        className="img-circle"
+        src={props.profilePicture?.valueOf() || noProfilePicture}
+      />
+    </div>
+  ));
+
 const googleIcon =
   "https://resources.finalsite.net/images/f_auto,q_auto,t_image_size_1/v1533665793/ogdensdorg/p6ckkwxkxi8zd03ja8h0/GoogleButton.png";
+
 const noProfilePicture = "https://simpleicon.com/wp-content/uploads/user1.png";
 
 const User = (props: Props) => {
@@ -30,41 +50,34 @@ const User = (props: Props) => {
   if (props.isLoggedIn) {
     return (
       <div className="qui-header-user center-vertically justify-content-end pr-2 pr-md-5">
-        <div
-          data-tip
-          data-for="header-user-menu"
-          data-event="click"
-          className="qui-user-icon-container"
-        >
-          <img
-            className="img-circle"
-            src={props.profilePicture?.valueOf() || noProfilePicture}
-          />
-        </div>
-        <ReactTooltip
-          id="header-user-menu"
-          clickable={true}
-          place="bottom"
-          effect="solid"
-          type="light"
-          globalEventOff="click"
-        >
-          <Link
-            to={`/${props.role.toLowerCase()}s/${props.userId}`}
-            style={{ textDecoration: "none" }}
-          >
-            <div className="qui-text font-size-18-md">Mi perfil</div>
-          </Link>
-          <div
-            className="qui-text font-size-18-md cursor-pointer"
-            onClick={() => {
-              clearSession(props.token);
-              props.history.go(0);
-            }}
-          >
-            Cerrar sesión
-          </div>
-        </ReactTooltip>
+        <Dropdown>
+          <Dropdown.Toggle as={CustomToggle(props)} />
+          <Dropdown.Menu>
+            <Dropdown.Item>
+              <Link
+                className="normalize-link"
+                to={`/${props.role.toLowerCase()}s/${props.userId}`}
+                style={{ textDecoration: "none" }}
+              >
+                <div className="qui-text font-size-18-md">
+                  <FontAwesomeIcon icon={faUser} /> Mi perfil
+                </div>
+              </Link>
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Item>
+              <div
+                className="qui-text font-size-18-md cursor-pointer"
+                onClick={() => {
+                  clearSession(props.token);
+                  props.history.go(0);
+                }}
+              >
+                <FontAwesomeIcon icon={faSignOutAlt} /> Cerrar sesión
+              </div>
+            </Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
       </div>
     );
   }
