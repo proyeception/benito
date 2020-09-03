9a5292c1d9ec95e3d8ec3be41aa97834cc2c91f3
Alejandro <aleperaltabazas@hotmail.com>
2020-09-03 11:36:37 -0300

    Agrego componente de SlideUp

---
+++ benito-frontend/src/components/Common/SlideUp.tsx
@@ -0,0 +1,27 @@
+import React, { ReactNode } from "react";
+import { hot } from "react-hot-loader";
+import "./styles.scss";
+import { motion } from "framer-motion";
+
+type Props = {
+  children: ReactNode;
+};
+
+const variants = {
+  in: {
+    opacity: 1,
+    y: 0,
+  },
+  out: {
+    opacity: 0,
+    y: "-100%",
+  },
+};
+
+const SlideUp = (props: Props) => (
+  <motion.div initial="out" animate="in" exit="out" variants={variants}>
+    {props.children}
+  </motion.div>
+);
+
+export default hot(module)(SlideUp);
