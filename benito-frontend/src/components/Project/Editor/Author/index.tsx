24f60f90fe5611ab84039bc7269279fbf91c3b8e
Alejandro <aleperaltabazas@hotmail.com>
2020-09-08 21:58:10 -0300

    Cambios en el layout

--- benito-frontend/src/components/Project/Editor/Author/index.tsx
+++ benito-frontend/src/components/Project/Editor/Author/index.tsx
@@ -33,99 +33,91 @@ const AuthorEdit = (props: Props) => {
   return (
     <SlideUp className="pt-5 pb-5">
       <div className="container bg-white p-3 p-md-5">
-        <div className="row flex-column-reverse flex-md-row">
-          <div className="col-12 col-md-8">
-            <div className="font-size-18 font-size-24-md">
-              <div className="font-weight-bolder mb-2 mb-md-2">Título</div>
-              <div className="font-weight-lighter">
-                <input
-                  type="text"
-                  name="title"
-                  className="form-control"
-                  value={title.valueOf()}
-                  onChange={setValues}
-                />
-              </div>
-            </div>
-            <div className="font-size-18 font-size-24-md mt-2 mt-md-4">
-              <div className="font-weight-bolder mb-2 mb-md-2">Descripción</div>
-              <div className="font-weight-lighter">
-                <textarea
-                  name="description"
-                  className="form-control"
-                  value={description.valueOf()}
-                  onChange={setValues}
-                  rows={5}
-                />
-              </div>
-            </div>
-            <div className="font-size-18 font-size-24-md mt-2 mt-md-4">
-              <div className="font-weight-bolder mb-2 mb-md-2">
-                Contenido extra
-              </div>
-              <Tabs defaultActiveKey="edit">
-                <Tab
-                  eventKey="edit"
-                  title="Editar"
-                  className="qui-project-edit-extra-content-container"
-                >
-                  <div className="font-weight-lighter h-100">
-                    <textarea
-                      name="extraContent"
-                      className="form-control h-100"
-                      value={extraContent.valueOf()}
-                      onChange={setValues}
-                    />
-                  </div>
-                </Tab>
-
-                <Tab
-                  eventKey="preview"
-                  title="Preview"
-                  className="qui-project-edit-extra-content-container"
-                >
-                  <ReactMarkdown
-                    className="font-weight-light border-right border-left border-bottom pl-md-3 pr-md-3 p-md-2 h-100"
-                    source={props.project.extraContent.valueOf()}
-                    renderers={{ image: Render }}
-                  />
-                </Tab>
-              </Tabs>
-            </div>
-          </div>
-          <div className="col-12 col-md-4 mt-3 mt-md-0">
-            <Image project={props.project} />
-            <Documents project={props.project} />
+        <Image project={props.project} />
+        <div className="font-size-18 font-size-24-md">
+          <div className="font-weight-bolder mb-2 mb-md-2">Título</div>
+          <div className="font-weight-lighter">
+            <input
+              type="text"
+              name="title"
+              className="form-control"
+              value={title.valueOf()}
+              onChange={setValues}
+            />
           </div>
         </div>
-        <div className="mt-5">
-          <Separator
-            display="d-block d-md-none"
-            color="gray"
-            marginLeft={0}
-            marginRight={0}
-          />
-          <div className="container d-flex center justify-content-around pt-3 pb-3 pt-md-5 pb-md-5 font-size-32 font-size-24-md">
-            <SaveChanges
-              title={title}
-              description={description}
-              extraContent={extraContent}
-              id={props.project.id}
-              className="d-none d-md-block cursor-pointer"
+        <div className="font-size-18 font-size-24-md mt-2 mt-md-4">
+          <div className="font-weight-bolder mb-2 mb-md-2">Descripción</div>
+          <div className="font-weight-lighter">
+            <textarea
+              name="description"
+              className="form-control"
+              value={description.valueOf()}
+              onChange={setValues}
+              rows={5}
+            />
+          </div>
+        </div>
+        <Documents project={props.project} />
+        <div className="font-size-18 font-size-24-md mt-2 mt-md-4">
+          <div className="font-weight-bolder mb-2 mb-md-2">Contenido extra</div>
+          <Tabs defaultActiveKey="edit">
+            <Tab
+              eventKey="edit"
+              title="Editar"
+              className="qui-project-edit-extra-content-container"
             >
-              <FontAwesomeIcon icon={faSave} color="green" /> Guardar cambios
-            </SaveChanges>
-            <DiscardChanges
-              className="d-none d-md-block cursor-pointer"
-              id={props.project.id}
+              <div className="font-weight-lighter h-100">
+                <textarea
+                  name="extraContent"
+                  className="form-control h-100"
+                  value={extraContent.valueOf()}
+                  onChange={setValues}
+                />
+              </div>
+            </Tab>
+
+            <Tab
+              eventKey="preview"
+              title="Preview"
+              className="qui-project-edit-extra-content-container"
             >
-              <FontAwesomeIcon icon={faTrash} color="red" /> Descartar
-            </DiscardChanges>
-            <div className="d-block d-md-none">
-              <FontAwesomeIcon icon={faSave} color="green" />
-            </div>
-            <div className="d-block d-md-none">
-              <FontAwesomeIcon icon={faTrash} color="red" />
+              <ReactMarkdown
+                className="font-weight-light border-right border-left border-bottom pl-md-3 pr-md-3 p-md-2 h-100"
+                source={props.project.extraContent.valueOf()}
+                renderers={{ image: Render }}
+              />
+            </Tab>
+          </Tabs>
+          <div className="mt-3">
+            <Separator
+              display="d-block d-md-none"
+              color="gray"
+              marginLeft={0}
+              marginRight={0}
+            />
+            <div className="container d-flex center justify-content-around pt-3 pb-3 pt-md-5 pb-md-5 font-size-32 font-size-24-md">
+              <SaveChanges
+                title={title}
+                description={description}
+                extraContent={extraContent}
+                id={props.project.id}
+                className="d-none d-md-block cursor-pointer"
+              >
+                <FontAwesomeIcon icon={faSave} color="green" /> Guardar cambios
+              </SaveChanges>
+              <DiscardChanges
+                className="d-none d-md-block cursor-pointer"
+                id={props.project.id}
+              >
+                <FontAwesomeIcon icon={faTrash} color="red" /> Descartar
+              </DiscardChanges>
+              <div className="d-block d-md-none">
+                <FontAwesomeIcon icon={faSave} color="green" />
+              </div>
+              <div className="d-block d-md-none">
+                <FontAwesomeIcon icon={faTrash} color="red" />
+              </div>
             </div>
           </div>
         </div>
