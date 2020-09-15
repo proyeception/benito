039d57fa50dccaf2d35d9cdb0408088381217084
Alejandro <aleperaltabazas@hotmail.com>
2020-09-15 19:17:17 -0300

    Agrego index.d.ts

---
+++ benito-frontend/src/index.d.ts
@@ -0,0 +1,8 @@
+declare module "react-alert-template-basic" {
+  var AlertTemplate: any;
+  export default AlertTemplate;
+}
+
+declare module "react-loading-overlay" {
+  export default class LoadingOverlay extends React.Component<any> {}
+}
