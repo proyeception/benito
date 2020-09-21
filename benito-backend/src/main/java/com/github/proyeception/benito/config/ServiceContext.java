0000ba35abff6f1ea4184db6e43edf7ae4e4c76a
Alejandro <aleperaltabazas@hotmail.com>
2020-09-21 20:41:27 -0300

    Fix configuración de mvc

--- benito-backend/src/main/java/com/github/proyeception/benito/config/ServiceContext.java
+++ benito-backend/src/main/java/com/github/proyeception/benito/config/ServiceContext.java
@@ -5,13 +5,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Import({
     ClientModule.class,
     ConfigModule.class,
     ConnectionModule.class,
-    CorsConfiguration.class,
+    MvcConfig.class,
     OAuthModule.class,
     ObjectMapperModule.class,
     ParserModule.class,
@@ -21,7 +20,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 })
 @ComponentScan("com.github.proyeception.benito")
 @Configuration
-@EnableWebMvc
 public class ServiceContext {
     @Bean(name = "multipartResolver")
     public CommonsMultipartResolver multipartResolver() {
