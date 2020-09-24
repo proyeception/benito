c3a8cc1817d92dad2cac189cf17c0a0ee649aef8
Alejandro <aleperaltabazas@hotmail.com>
2020-09-23 23:46:37 -0300

    Pequeños fixes de UX con loaders

--- benito-frontend/src/components/Home/index.tsx
+++ benito-frontend/src/components/Home/index.tsx
@@ -1,43 +1,58 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { hot } from "react-hot-loader";
 import "./styles.scss";
 import CategoriesSearchCarousel from "./CategoriesSearchCarousel";
 import HomeSearchBox from "./HomeSearchBox";
 import FeaturedGallery from "./FeaturedGallery";
 import Proyectate from "./Proyectate";
+import FadeIn from "../Common/FadeIn";
 import { RootState } from "../../reducers";
 import { connect } from "react-redux";
-import FadeIn from "../Common/FadeIn";
+import store from "../../store";
+import { toggleLoading } from "../../actions/common";
+import { AxiosRequestConfig, AxiosResponse } from "axios";
+import { benitoHost } from "../../config";
+import axios from "axios";
+import { Project } from "../../types";
+import { updateFeaturedProjects } from "../../actions/home";
 
 type Props = {
-  name: String;
-  category: String;
-  fromDate: String;
-  toDate: String;
+  loading: Boolean;
 };
 
-class Home extends React.Component<Props> {
-  constructor(props: Props, ctx: any) {
-    super(props, ctx);
-    this.state = {};
+const Home = (props: Props) => {
+  useEffect(() => {
+    store.dispatch(toggleLoading(true));
+    let config: AxiosRequestConfig = {
+      method: "GET",
+      url: `${benitoHost}/benito/projects/featured`,
+    };
+
+    axios(config)
+      .then((response: AxiosResponse<Array<Project>>) => response.data)
+      .then((projects) => store.dispatch(updateFeaturedProjects(projects)))
+      .then(() => store.dispatch(toggleLoading(false)))
+      .catch(console.error);
+  }, []);
+
+  if (props.loading) {
+    return <div></div>;
   }
 
-  render() {
-    return (
-      <FadeIn className="pt-5">
-        <HomeSearchBox
-          setDoRedirect={() => this.setState({ doRedirect: true })}
-        />
-        <FeaturedGallery />
-        <CategoriesSearchCarousel />
-        <Proyectate />
-      </FadeIn>
-    );
-  }
-}
+  return (
+    <FadeIn className="pt-5">
+      <HomeSearchBox />
+      <FeaturedGallery />
+      <CategoriesSearchCarousel />
+      <Proyectate />
+    </FadeIn>
+  );
+};
 
 const mapStateToProps = (rootState: RootState) => {
-  return rootState.search;
+  return {
+    loading: rootState.common.loading,
+  };
 };
 
 export default hot(module)(connect(mapStateToProps)(Home));
