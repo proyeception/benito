18a181ea87e8323002899a1886b42a6da6ec4e0d
Alejandro <aleperaltabazas@hotmail.com>
2020-09-25 16:50:56 -0300

    Agregando el theme...

--- benito-frontend/src/App.tsx
+++ benito-frontend/src/App.tsx
@@ -1,87 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { hot } from "react-hot-loader";
-import "./styles.scss";
-import Header from "./components/Header/index";
-import Footer from "./components/Footer";
-import { Switch, Route } from "react-router-dom";
-import Search from "./components/Search";
-import Home from "./components/Home";
-import axios, { AxiosRequestConfig } from "axios";
-import { benitoHost } from "./config";
-import { Category } from "./types";
-import store from "./store";
-import { updateCategories } from "./actions/common";
-import { AnimatePresence } from "framer-motion";
-import HamburgerMenu from "./components/Header/HamburgerMenu";
-import ViewProject from "./components/Project/index";
-import NotFound from "./components/NotFound";
-import Author from "./components/User/Author";
-import Supervisor from "./components/User/Supervisor";
-import SupervisorLogin from "./components/Login/SupervisorLogin";
-import ProjectEditor from "./components/Project/Editor";
-import Me from "./components/User/Me";
-import { openLocalStoredSession } from "./functions/session";
-import LoadingOverlay from "react-loading-overlay";
-import { RootState } from "./reducers";
-import { connect } from "react-redux";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Badge from './components/Badge/Badge';
 
-type Props = {
-  loading: Boolean;
-};
-
-const App = (props: Props) => {
-  const [isLoggingIn, setIsLoggingIn] = useState(true);
-  useEffect(() => {
-    let config: AxiosRequestConfig = {
-      method: "GET",
-      url: `${benitoHost}/benito/categories`,
-    };
-
-    openLocalStoredSession(setIsLoggingIn);
-
-    axios
-      .request<Array<Category>>(config)
-      .then((res) => res.data)
-      .then((categories) => store.dispatch(updateCategories(categories)))
-      .catch(console.error);
-  }, []);
-
-  if (isLoggingIn) {
-    return <div id="login" />;
-  }
-
+function App() {
   return (
-    <div className="h-100">
-      <LoadingOverlay active={props.loading.valueOf()} spinner>
-        <HamburgerMenu />
-        <Header />
-        <AnimatePresence>
-          <Switch>
-            <Route exact path="/projects/:projectId" component={ViewProject} />
-            <Route
-              exact
-              path="/projects/:projectId/edit"
-              component={ProjectEditor}
-            />
-            <Route exact path="/" component={Home} />
-            <Route exact path="/search" component={Search} />
-            <Route exact path="/authors/:userId" component={Author} />
-            <Route exact path="/supervisors/:userId" component={Supervisor} />
-            <Route exact path="/me/:tab" render={() => <Me />} />
-            <Route exact path="/supervisor/login" component={SupervisorLogin} />
-            <Route path="/*" component={NotFound} />
-          </Switch>
-        </AnimatePresence>
-        <Footer />
-      </LoadingOverlay>
+    <div className="App">      
+        <Badge>Hello</Badge>
     </div>
   );
-};
+}
 
-const mapStateToProps = (rootState: RootState) => {
-  return {
-    loading: rootState.common.loading,
-  };
-};
-
-export default hot(module)(connect(mapStateToProps)(App));
+export default App;
