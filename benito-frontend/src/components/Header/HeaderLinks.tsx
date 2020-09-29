e8aabe1000744c0972a7b5e56d33cc7233748dcc
Alejandro <aleperaltabazas@hotmail.com>
2020-09-28 22:38:05 -0300

    Pequeños ajustes de ux

--- benito-frontend/src/components/Header/HeaderLinks.tsx
+++ benito-frontend/src/components/Header/HeaderLinks.tsx
@@ -9,7 +9,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 
 // @material-ui/icons
-import { ExitToApp } from "@material-ui/icons";
+import { ExitToApp, MeetingRoom, Person, Settings } from "@material-ui/icons";
 
 // core components
 import Button from "../CustomButtons/Button";
@@ -18,13 +18,11 @@ import styles from "../../assets/jss/material-kit-react/components/headerLinksSt
 import { hot } from "react-hot-loader";
 import { RootState } from "../../reducers";
 import { connect } from "react-redux";
-import classNames from "classnames";
-import { Role } from "../../types";
 import UserLink from "../Links/UserLink";
 import CustomDropdown from "../CustomDropdown/CustomDropdown";
-import { invalidateSession } from "../../actions/session";
 import { clearSession } from "../../functions/session";
 import { LoggedInState, SessionState } from "../../store/session/types";
+import classNames from "classnames";
 
 const useStyles = makeStyles(styles);
 
@@ -40,20 +38,37 @@ const HeaderLinks = (props: Props) => {
       <ListItem className={classes.listItem}>
         {props.session.isLoggedIn ? (
           <CustomDropdown
-            buttonText="Mi cuenta"
+            buttonProps={{
+              color: "transparent",
+            }}
+            buttonText={props.session.fullName}
             dropdownList={[
-              <UserLink role={props.session.role} id={props.session.userId}>
-                Mi perfil
+              <UserLink
+                role={props.session.role}
+                id={props.session.userId}
+                className={classes.alignCenter}
+              >
+                <Person />
+                <span style={{ paddingLeft: "15px" }}>Mi perfil</span>
               </UserLink>,
+              <Link
+                to="/me/profile"
+                className={classNames("normalize-link", classes.alignCenter)}
+              >
+                <Settings />{" "}
+                <span style={{ paddingLeft: "15px" }}>Configuración</span>
+              </Link>,
               { divider: true },
               <div
+                className={classes.alignCenter}
                 onClick={() => {
                   clearSession((props.session as LoggedInState).token);
                   props.history.push("/");
                   props.history.go(0);
                 }}
               >
-                Cerrar sesión
+                <MeetingRoom />{" "}
+                <span style={{ paddingLeft: "15px" }}>Cerrar sesión</span>
               </div>,
             ]}
           />
