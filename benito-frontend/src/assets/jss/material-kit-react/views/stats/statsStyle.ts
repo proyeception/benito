e6a1c0edb9f1dc06f10134ffc380774fc1daff93
lu <lucia_roldan@outlook.com>
2020-10-24 21:28:49 -0300

    Cambia los estilos del grafico ded torta

---
+++ benito-frontend/src/assets/jss/material-kit-react/views/stats/statsStyle.ts
@@ -0,0 +1,45 @@
+import { title } from "../../../material-kit-react";
+import { Theme, createStyles } from "@material-ui/core/styles";
+
+const productStyle = (theme: Theme) => createStyles({
+  section: {
+    padding: "70px 0",
+    [theme.breakpoints.down("md")]: {
+      padding: "20px 0"
+    },
+    textAlign: "justify",
+  },
+  title: {
+    ...title,
+    marginBottom: "1rem",
+    marginTop: "30px",
+    minHeight: "32px",
+    textDecoration: "none",
+    overflow: "hidden",
+    overflowWrap: "anywhere",
+    textTransform: "uppercase",
+    fontSize: "30px",
+    textAlign: "center",
+    lineHeight: "initial",
+    [theme.breakpoints.down("md")]: {
+        fontSize: "4vw"
+    },
+    [theme.breakpoints.down("sm")]: {
+      fontSize: "6vw"
+  },
+  },
+  description: {
+    color: "#999",
+    overflow: "auto",
+    fontSize: "100%",
+  },
+  longTitle:{
+    textOverflow: "ellipsis",
+    lineClamp: 4,
+    display: "-webkit-box",
+    boxOrient: "vertical",
+    overflow: "hidden"
+  }
+});
+
+export default productStyle;
