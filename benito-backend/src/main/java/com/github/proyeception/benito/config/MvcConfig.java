ed8a844265466b7195141e04f30678ed160b95e7
Alejandro <aleperaltabazas@hotmail.com>
2020-10-30 20:07:48 -0300

    Agrego property source

--- benito-backend/src/main/java/com/github/proyeception/benito/config/MvcConfig.java
+++ benito-backend/src/main/java/com/github/proyeception/benito/config/MvcConfig.java
@@ -2,6 +2,7 @@ package com.github.proyeception.benito.config;
 
 import com.typesafe.config.Config;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @Configuration
+@PropertySource("classpath:application.properties")
 public class MvcConfig implements WebMvcConfigurer {
     private List<String> allowedOrigins;
 
