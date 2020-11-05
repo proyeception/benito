90bd3cbb265095747c2026f08eb07f118b564f40
Alejandro <aleperaltabazas@hotmail.com>
2020-11-04 21:02:07 -0300

    rip test rip staging

--- benito-frontend/environments/local/config.ts
+++ benito-frontend/environments/local/config.ts
@@ -1,3 +1,3 @@
-export const benitoHost = "";
+export const benitoHost = "http://localhost:9290";
 export const googleClientId =
   "1027287286648-h7b795dv6h356ngrrdavs88tgp1puc2o.apps.googleusercontent.com";
