2213db759de98909c383ddf9bf44686461ebe5da
gcastineira <gcastineira@exisoft.com.ar>
2020-11-07 19:42:48 -0300

    bot botoncito

--- benito-frontend/src/components/Bot/Proyectabot.tsx
+++ benito-frontend/src/components/Bot/Proyectabot.tsx
@@ -4,6 +4,9 @@ import {Chatbot} from "react-chatbot-kit"
 import config from "./config"
 import ActionProvider from "./ActionProvider"
 import MessageParser from "./MessageParser"
+import Fab from '@material-ui/core/Fab';
+import UpIcon from '@material-ui/icons/KeyboardArrowUp';
+import Collapse from '@material-ui/core/Collapse';
 
 import "../../assets/scss/bot/bot.scss"
 
@@ -11,20 +14,24 @@ const Proyectabot = (props: any) => {
 
     const [showChatbot, toggleChatbot] = useState(true);
 
-    return(<div> <div className="app-chatbot-container">
-            <Chatbot
-              config={config}
-              messageParser={MessageParser}
-              actionProvider={ActionProvider}
-              
-            />
-      </div>
+    const handleClick = () => {
+      toggleChatbot((showChatbot) => !showChatbot);
+    };
 
-      <button
-        className="app-chatbot-button"
-        onClick={() => toggleChatbot((prev) => !prev)}
-      >asdasdasd
-      </button>
+    return(
+    <div> 
+      <div className = "app-chatbot-button">
+      <Fab color = "secondary" aria-label= "expand" onClick = {handleClick}>
+        <UpIcon />
+      </Fab>
+      </div>
+      <Collapse in = {!showChatbot} className = "app-chatbot-container">
+                <Chatbot
+                  config={config}
+                  messageParser={MessageParser}
+                  actionProvider={ActionProvider}            
+                />
+        </Collapse>
   </div>
 );
         }
