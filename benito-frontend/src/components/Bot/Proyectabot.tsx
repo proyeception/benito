2773338b6e144cfe6bcc80c48127b565606f3304
lu <lucia_roldan@outlook.com>
2020-11-07 19:00:50 -0300

    Comienza a agregar estilos por universidad

--- benito-frontend/src/components/Bot/Proyectabot.tsx
+++ benito-frontend/src/components/Bot/Proyectabot.tsx
@@ -1,32 +1,95 @@
 import React, { useState } from "react"
 //import { ConditionallyRender } from "react-util-kit";
 import {Chatbot} from "react-chatbot-kit"
-import config from "./config"
 import ActionProvider from "./ActionProvider"
 import MessageParser from "./MessageParser"
 
 import "../../assets/scss/bot/bot.scss"
+import { RootState } from "../../reducers"
+import { hot } from "react-hot-loader"
+import { connect } from "react-redux"
+import { RouteComponentProps, withRouter } from "react-router-dom"
+import { SessionState } from "../../store/session/types"
+import { createChatBotMessage } from "react-chatbot-kit";
+import GeneralOptions from "./widgets/GeneralOptions/GeneralOptions";
+import CategoriesOptions from "./widgets/GeneralOptions/CategoriesOptions";
+import ProjectOptions from "./widgets/GeneralOptions/ProjectOptions";
 
-const Proyectabot = (props: any) => {
+interface ProyectabotProps extends RouteComponentProps {
+  session?: SessionState;
+}
 
-    const [showChatbot, toggleChatbot] = useState(true);
+const Proyectabot = (props: ProyectabotProps) => {
 
-    return(<div> <div className="app-chatbot-container">
-            <Chatbot
-              config={config}
-              messageParser={MessageParser}
-              actionProvider={ActionProvider}
-              
-            />
+  let color: string = "#c41234"
+  if(props.session && props.session.isLoggedIn  && props.session.selectedOrganization){
+    color = props.session.selectedOrganization.color
+  }
+
+  const botName = "Proyectabot";
+
+  const config = {
+    botName: botName,
+    lang: "es",
+    customStyles: {
+      botMessageBox: {
+        backgroundColor: color,
+      },
+      chatButton: {
+        backgroundColor: color,
+      },
+    },
+    initialMessages: [
+      createChatBotMessage(`Hola, soy ${botName}. ¿En qué te puedo ayudar?`,
+      {
+        widget: "generalOptions",
+        delay: 500,
+      }
+      ),
+    ],
+    widgets: [
+      {
+        widgetName: "generalOptions",
+        widgetFunc: (props:any) => <GeneralOptions {...props} />,
+      },
+      {
+        widgetName: "categoriesOptions",
+        widgetFunc: (props:any) => <CategoriesOptions {...props} />,
+      },
+      {
+        widgetName: "ProjectOptions",
+        widgetFunc: (props:any) => <ProjectOptions {...props} />,
+      },
+    ],
+  };
+
+  const [showChatbot, toggleChatbot] = useState(true);
+
+  return(
+    <div> 
+      <div className="app-chatbot-container">
+        <Chatbot
+          config={config}
+          messageParser={MessageParser}
+          actionProvider={ActionProvider}
+          
+        />
       </div>
-
       <button
         className="app-chatbot-button"
         onClick={() => toggleChatbot((prev) => !prev)}
       >asdasdasd
       </button>
-  </div>
-);
-        }
+    </div>
+  );
+}
 
-export default Proyectabot
+
+const mapStateToProps = (rootState: RootState) => {
+  return {
+    session: rootState.session,
+  };
+};
+
+export default hot(module)(connect(mapStateToProps)(withRouter(Proyectabot)));
+
--- benito-frontend/src/components/Bot/widgets/GeneralOptions/GeneralOptions.tsx
+++ benito-frontend/src/components/Bot/widgets/GeneralOptions/GeneralOptions.tsx
@@ -1,13 +1,28 @@
 import React from "react";
+import { hot } from "react-hot-loader";
+import { connect } from "react-redux";
+import { RouteComponentProps, withRouter } from "react-router-dom";
+import { RootState } from "../../../../reducers";
+import { SessionState } from "../../../../store/session/types";
 
 import Options from "../Options/Options";
 
-const GeneralOptions = (props:any) => {
+interface GeneralOptionsProps extends RouteComponentProps {
+  session?: SessionState;
+}
+
+const GeneralOptions = (props: GeneralOptionsProps | any) => {
+
+  let color: string = "#c41234"
+  if(props.session && props.session.isLoggedIn  && props.session.selectedOrganization){
+    color = props.session.selectedOrganization.color
+  }
+
   const options = [
     {
       name: "Estoy buscando un proyecto",
       handler: props.actionProvider.handleCategoriesChoice,
-      id: 1
+      id: 1,
     },
     {
       name: "¿Qué es proyectate?",
@@ -27,10 +42,17 @@ const GeneralOptions = (props:any) => {
     {
       name: "¿Cómo cargo un proyecto?",
       handler: props.actionProvider.handleProjectUploadQuestion,
-      id: 5
+      id: 5,
     } 
   ];
-  return <Options options={options} {...props} />;
+  return <Options options={options} color={color} {...props} />;
 };
 
-export default GeneralOptions;
+const mapStateToProps = (rootState: RootState) => {
+
+  return {
+    session: rootState.session,
+  };
+};
+
+export default hot(module)(connect(mapStateToProps)(withRouter(GeneralOptions)));
