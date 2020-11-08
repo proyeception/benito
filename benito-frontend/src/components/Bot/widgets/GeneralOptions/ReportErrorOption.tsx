966be9029fa8f64eba0aa697cd6cf3a8de33430d
lu <lucia_roldan@outlook.com>
2020-11-08 10:23:00 -0300

    Cambia estilo de mailto

--- benito-frontend/src/components/Bot/widgets/GeneralOptions/ReportErrorOption.tsx
+++ benito-frontend/src/components/Bot/widgets/GeneralOptions/ReportErrorOption.tsx
@@ -16,23 +16,9 @@ const ReportErrorOption = (props: ReportErrorOptionsProps | any) => {
     color = props.session.selectedOrganization.color
   }
 
-  const addLight = function(color: string, amount: number){
-    let cc = parseInt(color,16) + amount;
-    let c = (cc > 255) ? 255 : (cc);
-    return (c.toString(16).length > 1 ) ? c.toString(16) : `0${c.toString(16)}`.toString();
-  }
-
-  const lighten = (color: string, amount: number)=> {
-    color = (color.indexOf("#")>=0) ? color.substring(1,color.length) : color;
-    amount = parseInt(((255*amount)/100).toString());
-    return color = `#${addLight(color.substring(0,2), amount)}${addLight(color.substring(2,4), amount)}${addLight(color.substring(4,6), amount)}`;
-  }
-
-  color = lighten(color, 20)
-
   return <div style={{textAlign: "center"}}>
     <a href="mailto:proyeception@gmail.com" style={{color: "#2f3336"}}>
-      <button style={{backgroundColor:color, color: "white", borderColor:color, cursor: "pointer", padding: "10px", borderRadius: "6px", margin: "auto"}}>O podes hacer click acá</button>
+      <button style={{backgroundColor:"white", color: color, borderColor:color, cursor: "pointer", padding: "10px", borderRadius: "6px", margin: "auto"}}>O podes hacer click acá</button>
     </a>
   </div>;
 };
